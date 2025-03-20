351b12cc768570953ab2a6e89412acac3749e0f1
Anshuman Tripathy <113134740+Anshuu0046@users.noreply.github.com>
2025-03-20 23:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# Add this for Vercel
-app = app
-
+# No need for app.run() in production
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
